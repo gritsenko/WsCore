5c6740e115a6ad1b2d42f27fa2d3ff301df06dee
igor.gritsenko <agent190r@yandex.ru>
2025-10-14 00:27:28 +0300

    Project structure refactoring

--- Source/WsServer/Game.Protocol/ServerMessageTypeAttribute.cs
+++
@@ -1,6 +0,0 @@
-﻿namespace Game.Protocol;
-
-public class ServerMessageTypeAttribute(ServerMessageType serverMessageType) : Attribute
-{
-    public ServerMessageType ServerMessageType { get; } = serverMessageType;
-}
---
+++ WsServer/Game/Protocol/Map/Events/UpdateMapObjectsEvent.cs
@@ -0,0 +1,17 @@
+﻿using System.Linq;
+using Game.Core.World;
+using WsServer.Abstract.Messages;
+
+namespace Game.ServerLogic.Map.Events;
+
+public struct UpdateMapObjectsEvent : IServerEvent
+{
+    public static byte TypeId => 52;
+
+    public MapObjectData[] MapObjects;
+
+    public UpdateMapObjectsEvent(GameObject[] objects)
+    {
+        MapObjects = objects.Select(x => new MapObjectData(x)).ToArray();
+    }
+}
